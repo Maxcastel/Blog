77da2f567ab4776021db2432536f270aab2f22d4
Maxence Castel <maxence.castel59@gmail.com>
2023-03-01 21:31:08 +0100

    “commit”

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 
 /* INFORMATIONS DE CONNEXION A LA BDD */
-define('DB_NAME', 'exophp');
+define('DB_NAME', 'projetphp');
 define('DB_USER', 'root');
-define('DB_PWD', '');
+define('DB_PWD', 'aa');
 
 ?>
